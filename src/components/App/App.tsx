761cc819117a6ad6733cde2dd445edc0ddcbf20e
Goja <gortnar@beograd.com>
2021-08-05 11:43:36 +0200

    React - instalacija biblioteka Bootstrap, jQuery i Font Awesoe 05.08.2021.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.js'
+import 'popper.js/dist/popper.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css';
+import { Container, Card } from 'react-bootstrap';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
   return (
-    <div className="App">
-      Home
-    </div>
+    
+    <Container>
+      <Card bg = 'secondary' text = 'white'>
+      	<Card.Header>
+    		<FontAwesomeIcon icon = { faHome }></FontAwesomeIcon> Početna strana
+       	</Card.Header> 
+        <Card.Body>
+          <Card.Text>
+            Sadržaj
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 
